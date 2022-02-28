487d13205983a94e440dd4fadff33b0203f9fab8
jebetan <jebetan51@gmail.com>
2022-03-01 01:22:00 +0800

    doing register

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import { ParticlesOptions } from './Particles';
 import './App.css';
 
 // const app = new Clarifai.App({
-//   apiKey: 'e09cacd8320442fc808c207447464754'
-// })
+//   apiKey: '929ba1f9eb194f9096feb3936765591a'
+// });
 
 class App extends Component {
   constructor (props) {
@@ -24,8 +24,27 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        password: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
+
+loadUser = (data) => {
+  this.setState({ user: {
+    id: data.id,
+    name: data.name,
+    email: data.email,
+    password: data.password,
+    entries: data.entries,
+    joined: data.joined
+  }})
+}
 
 calculateFaceLocation = (data) => {
  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -52,10 +71,33 @@ this.setState({input: event.target.value});
 
 onButtonSubmit = () => {
   this.setState({imageUrl: this.state.input})
-  App.models.predict(
-    Clarifai.FACE_DETECT_MODEL,
-    this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+  App.models
+  .predict(
+   // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+    // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+    // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+    // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+    // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
+    // so you would change from:
+    // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    // to:
+    //.predict('929ba1f9eb194f9096feb3936765591a'
+        '929ba1f9eb194f9096feb3936765591a',
+        this.state.input)
+      .then(response => {
+        console.log('hi', response)
+        if (response) {
+          fetch('http://localhost:3001/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+        }
+        this.displayFaceBox
+        (this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err))
 }
 onRouteChange = (route) => {
@@ -89,7 +131,7 @@ onRouteChange = (route) => {
       : (
           route === 'signin'
           ? <Signin onRouteChange={this.onRouteChange} />
-          : <Register onRouteChange={this.onRouteChange} />
+          : <Register loadUser={ this.loadUser } onRouteChange={this.onRouteChange} />
         )       
       }
       </div>
